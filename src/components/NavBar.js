a870a4ba526bf17f5621f868bb2c7f24de45df1c
Michael Cruz <cmichael_12@yahoo.com>
2023-09-16 09:08:44 -0500

    cl1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
+const linkStyles = {
+  display: "inline-block",
+  margin: "0 6px 6px",
+  textDecoration: "underline",
+  color: "blue",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return ( <div className="navbar">
+  <ul>
+    <li>
+
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+      >
+        Home
+      </NavLink>
+    </li>
+    <li>
+
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+>
+        Movies 
+      </NavLink>
+    </li>
+    <li>
+      {" "}
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+    >
+        Directors
+      </NavLink>
+    </li>
+    <li>
+
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+
+      >
+        Actors 
+      </NavLink>
+    </li>
+  </ul>
+  </div>
+);
 }
 
 export default NavBar;
